d89e1f7d975ddfeb9ae436fc76234bee6d2c42e5
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-01-08 15:47:51 +0200

    create app

---
+++ src/app/companies/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import StatusLabel, { Status } from "../components/StatusLabel";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Companies</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+    </div>
+  );
+};
+
+export default page;
